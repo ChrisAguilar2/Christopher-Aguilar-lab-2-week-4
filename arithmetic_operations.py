aa0c3aa7431ecae511a0302b4fd7c4a70eb3f5dc
ChrisAguilar2 <aguilar.christoph520@student.ccm.edu>
2026-09-23 22:38:16 -0400

    Complete Week 4 Lab 2

---
+++ arithmetic_operations.py
@@ -0,0 +1,29 @@
+#Christopher Aguilar
+#CMP 131
+#Basic Arithmetic Operations Week 4 Lab 2
+#September 22, 2026
+
+# Program title
+print("==============================")
+print("Basic Arithmetic Operations")
+print("==============================")
+
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+power = first_number ** second_number
+average = (first_number + second_number) / 2
+
+# Output
+print("First Number: ", first_number)
+print("Second Number :", second_number)
+print(f"Addittion:  {addition:.2f}")
+print(f"Subtraction:  {subtraction:.2f}")
+print(f"Multiplication:  {multiplication:.2f}")
+print(f"Division:  {division:.2f}")
+print(f"Power:  {power:.2f}")
+print(f"Average:  {average:.2f}")
