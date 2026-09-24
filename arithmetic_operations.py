45d92f5949bf5169751a89819130bd1eb505a0d3
ChrisAguilar2 <aguilar.christoph520@student.ccm.edu>
2026-09-23 22:45:40 -0400

    Complete Week 4 Lab 2

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -8,9 +8,11 @@ print("==============================")
 print("Basic Arithmetic Operations")
 print("==============================")
 
+# User input
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
+# Arithmetic calculation 
 addition = first_number + second_number
 subtraction = first_number - second_number
 multiplication = first_number * second_number
